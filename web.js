b1f9c5b8750bf83d594a66bed75d726c72690273
rohithm87 <rohithm87@gmail.com>
2013-07-09 22:16:28 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var htmlFile = require('fs');
+
+var htmlCode = htmlFile.readFileSync("./index.html");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( htmlCode );
 });
 
 var port = process.env.PORT || 5000;
