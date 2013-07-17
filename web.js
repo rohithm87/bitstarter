befc6a7835fee2a62dd1069adec84be2329794ec
rohithm87 <rohithm87@gmail.com>
2013-07-16 21:50:19 -0700

    updated index.html - responsive design and listening on port 8080

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var htmlFile = require('fs');
 
-var htmlCode = htmlFile.readFileSync("./index.html");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var htmlCode = htmlFile.readFileSync("./index.html");
   response.send( htmlCode.toString() );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
